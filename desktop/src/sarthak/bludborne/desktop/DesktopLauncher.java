447f0f813c71491e2892435591f83acd1cc5b5d2
Sarthak <sarthakbh321@gmail.com>
2019-11-19 15:07:38 +0530

    changed desktop configs

--- desktop/src/sarthak/bludborne/desktop/DesktopLauncher.java
+++ desktop/src/sarthak/bludborne/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package sarthak.bludborne.desktop;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import sarthak.bludborne.Main;
@@ -7,6 +8,12 @@ import sarthak.bludborne.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+		config.title = "BludBorne";
+		config.resizable = false;
+		config.width = 800;
+		config.height = 600;
+		config.useGL30 = false;
+
+		new LwjglApplication(new Main(), config).setLogLevel(Application.LOG_DEBUG);
 	}
 }
