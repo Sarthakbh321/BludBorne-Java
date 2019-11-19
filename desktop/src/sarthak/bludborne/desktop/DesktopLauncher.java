a8e2d79ea61540c3e814007f5b69286545fe10ca
Sarthak <sarthakbh321@gmail.com>
2019-11-19 14:55:13 +0530

    Initialized libGDX project

---
+++ desktop/src/sarthak/bludborne/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package sarthak.bludborne.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import sarthak.bludborne.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
